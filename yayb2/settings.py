ef0a19312ed0d8ee6d298d3f25659250d4126263
beyonsay <73654501+beyonsay@users.noreply.github.com>
2023-08-11 23:27:43 +0200

    static file edit

--- yayb2/settings.py
+++ yayb2/settings.py
@@ -138,10 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR/ 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR/ 'yayb2/static')
+    os.path.join(BASE_DIR, 'yayb2/static')
 ]
 
 MEDIA_URL = 'media/'
