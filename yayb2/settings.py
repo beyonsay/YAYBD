2d29de316f6f15771c057dad08c1e54a6fa87e5b
beyonsay <73654501+beyonsay@users.noreply.github.com>
2023-08-11 23:38:45 +0200

    another static setting

--- yayb2/settings.py
+++ yayb2/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-t9kfn*tx^zoez)atz@3o%^)b&#j$1a573p8c680*nom5f$maot
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'yayb.onrender.com'] 
+ALLOWED_HOSTS = [ 'yayb.onrender.com', '127.0.0.1'] 
 
 
 # Application definition
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR/ 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'yayb2/static')
