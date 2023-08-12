94ec6c3390a56f6231f4e8eacd19107b9c6cca1e
beyonsay <73654501+beyonsay@users.noreply.github.com>
2023-08-12 21:00:29 +0200

    more settings changes

--- yayb2/settings.py
+++ yayb2/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [ 'yayb.onrender.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'render.apps.RenderConfig',
     'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,6 +145,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'yayb2/static')
 ]
+
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
